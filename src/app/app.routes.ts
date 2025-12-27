e0f93eb53bcabe202dd13882f5bb58205fb7e2fd
Debugg <debuggforbussiness@gmail.com>
2025-12-27 22:46:15 +0100

    Created a main layout with nav bar template for other pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { NotFound } from './pages/not-found/not-found';
+import { MainLayout } from './layouts/main-layout/main-layout';
 
 export const routes: Routes = [
-  { path: '', component: Home },
+  { path: '', component: MainLayout, children: [{ path: '', component: Home }] },
   { path: '**', component: NotFound },
 ];
