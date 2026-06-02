d9350b987b6c051dbedd2861c8271c1503957ed5
Naim Ghulam <naim.ghulam@wow-group.com>
2026-06-02 15:44:41 +0200

    Added navbar

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Controls } from '../../components/controls/controls';
+import { Navlinks } from "../../components/navlinks/navlinks";
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, Controls],
+  imports: [RouterOutlet, Controls, Navlinks, Footer],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
