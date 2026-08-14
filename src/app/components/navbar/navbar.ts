1a6ecce49e9d0d328d432234b3ce1b0410817085
Debugg <debuggforbussiness@gmail.com>
2026-08-14 23:26:25 +0200

    Added desktop nav bar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,16 +1,28 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NeonService } from '../../services/neon.service';
 import { Controls } from "../controls/controls";
 
+interface NavLink {
+  route: string;
+  label: string;
+}
+
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, Controls],
+  imports: [RouterLink, RouterLinkActive, Controls],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
   private readonly neonService = inject(NeonService);
+
+  navLinks: NavLink[] = [
+    { route: '/blog', label: 'Blog' },
+    { route: '/activity', label: 'Activity' },
+    { route: '/about', label: 'About me' },
+    { route: '/links', label: 'Resources' },
+  ];
 
   playHoverAudio(): void {
     this.neonService.playHover();
